27ce6979dade38e57f8e81715045f421442e6c7c
Daniel Kats <d.kats@fkf.mpg.de>
2023-08-02 21:24:59 +0200

    Add favicon

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,11 @@
 push!(LOAD_PATH,"../src/")
 using Documenter, ElemCo
 
-makedocs(sitename="ElemCo.jl documentation")
+makedocs(
+  modules = [ElemCo],
+  format = Documenter.HTML(
+    # Use clean URLs, unless built as a "local" build
+    prettyurls = !("local" in ARGS),
+    assets = ["assets/favicon.ico"],
+  ),
+  sitename="ElemCo.jl documentation")
